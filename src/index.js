1224e04f9eb7f00a92cfa9bd29de8f4bdbd59bc2
Asset Tazhibayev <gravitationether2020@gmail.com>
2022-02-18 04:26:51 +0600

    webpack begin file

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import '../index.css';
+import IMAGE from "../img/iam.jpg";
+
+const imgFirst = document.createElement('img');
+imgFirst.className = 'is-image';
+imgFirst.src = IMAGE;
+
+document.body.append(imgFirst);
